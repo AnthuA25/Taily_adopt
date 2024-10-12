69b7a0825b61f9f624a6e63bd4383ad65f6c3196
Barbara <0deabreubarbara@gmail.com>
2024-10-12 12:52:27 -0500

    feat: starting index.js initial config

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,1 +1,34 @@
-base
+import express from 'express';
+import dotenv from 'dotenv';
+import { sequelize } from './config/database';
+import { testConnection } from './utils/connection';
+
+
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(router)
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('¡Welcomeeeee');
+  });
+
+
+  const startServer = async () => {
+    try {
+      await testConnection();
+      console.log('Database connected');
+
+      await sequelize.sync();
+      console.log('Database synchronized successfully!');
+
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+};
+  
+startServer();
