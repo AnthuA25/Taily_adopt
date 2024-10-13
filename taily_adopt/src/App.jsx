0994891ec76f0b2e402e8557c8919c4cc6942cc1
Aylin <aylin_santacruz@hotmail.com>
2024-10-13 06:55:53 -0500

    feat:modify router

--- taily_adopt/src/App.jsx
+++ taily_adopt/src/App.jsx
@@ -1,22 +1,31 @@
 import { Login } from "./components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Register } from "./components/Register";
 import { HomeAdop } from "./components/adopt/HomeAdop";
 import { HomeRes } from "./components/rescuer/HomeRes";
 import { InfoPet } from "./components/adopt/InfoPet";
+import { AddPet } from "./components/rescuer/AddPet";
+
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/adoptante" element={<HomeAdop />} />
-          <Route path="/rescatista" element={<HomeRes />} />
-          <Route path="/info" elemet={<InfoPet/>}/>
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/adoptante" element={<HomeAdop />} />
+            <Route path="/rescatista" element={<HomeRes />} />
+            <Route path="/rescatista/nueva-publicacion" element={<AddPet />} />
+            <Route path="/info" elemet={<InfoPet />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
