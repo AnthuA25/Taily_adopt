3fd272d6aec76cdceda51b4588230c5d6128f115
Barbara <0deabreubarbara@gmail.com>
2024-10-12 22:16:12 -0500

    chore: add comments in routes

--- backEnd/src/routes/rescuerRoutes.js
+++ backEnd/src/routes/rescuerRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const rescuerController = require('../controllers/rescuerController')
 const { isAuthenticated } = require("../middleware");
 
-router.get('/followup', isAuthenticated, rescuerController.followUps)
+// Announcements
 router.get('/announcement', isAuthenticated, rescuerController.announcements);
 router.get('/announcement/:pet_id', isAuthenticated, rescuerController.getAnAnnouncements);
 router.post('/announcement', isAuthenticated, rescuerController.newPetAnnouncement);
+
+// Followups
+router.get('/followup', isAuthenticated, rescuerController.followUps)
 
 module.exports = router;
