41a4005442b8db63e95ab44525740b0888c039d9
Aylin <aylin_santacruz@hotmail.com>
2024-10-12 22:51:05 -0500

    feat: Add nav  to adop home

---
+++ taily_adopt/src/components/NavbarAdop.jsx
@@ -0,0 +1,13 @@
+import "../styles/NavbarAdop.css";
+
+export const NavbarAdop = () => {
+  return (
+    <div className="navbar">
+        <h3>Mascotas</h3>
+        <h3>Favoritos</h3>
+        <div className="user">
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
