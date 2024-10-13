dc461fd07e7c9b0fc610515496066641616d7e9c
Aylin <aylin_santacruz@hotmail.com>
2024-10-13 14:30:10 -0500

    feat:Implement email

--- taily_adopt/src/components/adopt/DetailPet.jsx
+++ taily_adopt/src/components/adopt/DetailPet.jsx
@@ -2,13 +2,39 @@ import { useState, useEffect } from "react";
 import { NavbarAdop } from "./NavbarAdop";
 import { useParams } from "react-router-dom";
 import { Loading } from "../Loading";
-import '../../styles/adopt/DetailPet.css';
+import "../../styles/adopt/DetailPet.css";
 
 export const DetailPet = () => {
   const { pet_id } = useParams(); // ID de la URL
   const [pet, setPet] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [rescuer, setRescuer] = useState(null);
+
+  const fetchRescuerDetails = async (user_id) => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch(`http://localhost:8000/users/${user_id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error al obtener los detalles del rescatista");
+      }
+      const userData = await response.json();
+      console.log("userpe",userData)
+      setRescuer(userData); // Guardar los detalles del rescatista
+    } catch (error) {
+      console.error(
+        "Error al obtener los detalles del rescatista:",
+        error.message
+      );
+    }
+  };
 
   const fetchPetDetails = async () => {
     try {
@@ -23,9 +49,9 @@ export const DetailPet = () => {
       }
 
       const data = await response.json();
-      console.log("data_",data)
+      console.log("data_", data);
       setPet(data);
-
+      fetchRescuerDetails(data.user_id);
     } catch (error) {
       console.error(
         "Error al obtener los detalles de la mascota:",
@@ -51,7 +77,8 @@ export const DetailPet = () => {
 
   //envia el correo
   const handleEmailClick = () => {
-    window.location.href = `mailto:${}`;
+    const subject = `Interesado en adoptar a ${pet.name}`;
+    window.open(`mailto:${rescuer.email}?subject=${encodeURIComponent(subject)}`, '_blank');
   };
 
   return (
@@ -60,12 +87,24 @@ export const DetailPet = () => {
       <img src={pet.photo_url} alt={pet.name} />
       <h1>{pet.name}</h1>
       <p>{pet.description}</p>
-      <div className="rescuer-info">
-        <h2>Información del Rescatista</h2>
-        <button onClick={handleEmailClick} className="email-button">
-          Enviar información
-        </button>
-      </div>
+      {rescuer && (
+        <div className="rescuer-info">
+          <h2>Información del Rescatista</h2>
+          <p>
+            <strong>Nombre:</strong> {rescuer.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {rescuer.email}
+          </p>
+          <p>
+            <strong>Teléfono:</strong> {rescuer.phone}
+          </p>
+
+          <button onClick={handleEmailClick} className="email-button">
+            Enviar información
+          </button>
+        </div>
+      )}
     </div>
   );
 };
