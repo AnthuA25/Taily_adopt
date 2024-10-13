214391b7bb3400e95eb0d799a1e7841eb82a157c
Aylin <aylin_santacruz@hotmail.com>
2024-10-13 10:32:12 -0500

    feat:Implement pets to adopt

--- taily_adopt/src/components/adopt/HomeAdop.jsx
+++ taily_adopt/src/components/adopt/HomeAdop.jsx
@@ -1,30 +1,46 @@
 import { NavbarAdop } from "./NavbarAdop.jsx";
 import "../../styles/adopt/HomeAdop.css";
 import patitas from "../../assets/patitas.png";
-import puppy from "../../assets/puppy.jpeg";
+// import puppy from "../../assets/puppy.jpeg";
 import { FaHeart } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const pets = [
-  { id: 1, name: "Fluffy", image: puppy, liked: false },
-  { id: 2, name: "Peep", image: puppy, liked: false },
-  { id: 3, name: "Bubbles", image: puppy, liked: false },
-  { id: 4, name: "Spot", image: puppy, liked: false },
-  { id: 5, name: "Mittens", image: puppy, liked: false },
-  { id: 6, name: "Kitty", image: puppy, liked: false },
-  { id: 7, name: "Golden", image: puppy, liked: false },
-];
+// const pets = [
+//   { id: 1, name: "Fluffy", image: puppy, liked: false },
+//   { id: 2, name: "Peep", image: puppy, liked: false },
+//   { id: 3, name: "Bubbles", image: puppy, liked: false },
+//   { id: 4, name: "Spot", image: puppy, liked: false },
+//   { id: 5, name: "Mittens", image: puppy, liked: false },
+//   { id: 6, name: "Kitty", image: puppy, liked: false },
+//   { id: 7, name: "Golden", image: puppy, liked: false },
+// ];
 
 export const HomeAdop = () => {
-  const [petList, setPetList] = useState(pets);
+  const [petList, setPetList] = useState([]);
 
-  // const toggleLike = (id) => {
-  //   setPetList((prevPets) =>
-  //     prevPets.map((pet) =>
-  //       pet.id === id ? { ...pet, liked: !pet.liked } : pet
-  //     )
-  //   );
-  // };
+  const fetchPets = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/account", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error al obtener las mascotas");
+      }
+      const data = await response.json();
+      setPetList(data); // Ajusta según el formato de tu respuesta
+      console.log(data)
+    } catch (error) {
+      console.error("Error al obtener las mascotas:", error.message);
+    }
+  };
+  // Usar useEffect para llamar fetchPets cuando el componente se monte
+  useEffect(() => {
+    fetchPets();
+  }, []);
+
   return (
     <div className="home-adop">
       <NavbarAdop />
@@ -33,15 +49,19 @@ export const HomeAdop = () => {
       </h3>
       <div className="container">
         <div className="all-pets">
-          {petList.map((pet) => (
-            <div key={pet.id} className="photo">
-              <img src={pet.image} alt={pet.name} />
-              <div className="pet-info">
-                <h3>{pet.name}</h3>
+          {petList.length > 0 ? (
+            petList.map((pet) => (
+              <div key={pet.id} className="photo">
+                <img src={pet.photo_url} alt={pet.name} />
+                <div className="pet-info">
+                  <h3>{pet.name}</h3>
                   <FaHeart />
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No hay mascotas disponibles para adopción.</p>
+          )}
         </div>
       </div>
     </div>
