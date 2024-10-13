8ba1ab4d8e35ed3538879df242e620f4ce5ab927
Aylin <aylin_santacruz@hotmail.com>
2024-10-13 13:25:34 -0500

    feat:add deatil to pet

--- taily_adopt/src/components/adopt/DetailPet.jsx
+++ taily_adopt/src/components/adopt/DetailPet.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { NavbarAdop } from "./NavbarAdop";
 import { useParams } from "react-router-dom";
 import { Loading } from "../Loading";
+import '../../styles/adopt/DetailPet.css';
 
 export const DetailPet = () => {
   const { pet_id } = useParams(); // ID de la URL
@@ -20,10 +21,11 @@ export const DetailPet = () => {
       if (!response.ok) {
         throw new Error("Error al obtener los detalles de la mascota");
       }
-      console.log(response);
+
       const data = await response.json();
-      console.log("data", data);
+      console.log("data_",data)
       setPet(data);
+
     } catch (error) {
       console.error(
         "Error al obtener los detalles de la mascota:",
@@ -47,13 +49,23 @@ export const DetailPet = () => {
     return <p>No se encontraron detalles de la mascota.</p>;
   }
 
-  return (
-    <div>
-      <NavbarAdop />
-      <h1>{pet.name}</h1>
-      <img src={pet.photo_url} alt={pet.name} />
+  //envia el correo
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${}`;
+  };
 
+  return (
+    <div className="detailPet">
+      <NavbarAdop />
+      <img src={pet.photo_url} alt={pet.name} />
+      <h1>{pet.name}</h1>
       <p>{pet.description}</p>
+      <div className="rescuer-info">
+        <h2>Información del Rescatista</h2>
+        <button onClick={handleEmailClick} className="email-button">
+          Enviar información
+        </button>
+      </div>
     </div>
   );
 };
